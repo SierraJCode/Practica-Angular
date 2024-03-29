9b5875bfaa6eb83fd5c0da1f3b8da8d9b87657c7
Jesus Sierra <sierraja06@gmail.com>
2024-03-29 10:37:06 -0500

    Se terminó el formulario de plantilla, con sus respectivos estados (validar ningún campo se vaya vacio)

---
+++ src/app/validacion/validacion.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-validacion',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './validacion.component.html',
+  styleUrl: './validacion.component.scss'
+})
+export class ValidacionComponent {
+  persona = {
+    usuario: "",
+    email: "",
+    date: "",
+
+    procesar(){
+      alert(this.date)
+    }
+  }
+  
+  procesarDatos(){
+    alert("Enviando datos..")
+  }
+
+  
+}
